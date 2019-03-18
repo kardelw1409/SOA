e3918799aa64fddb00ab963ca2332026b227e795
Yauheni Dzervayedau <Yauheni_Dzervayedau@epam.com>
2019-03-18 13:36:18 +0300

    Create CRUD for StudentService

---
+++ SOAProjects/SudentsService/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SudentsService.Models
+{
+    public class Student
+    {
+        [Key]
+        public int RegId { get; set; }
+
+        public int Course { get; set; }
+
+        public DateTime Enrolled { get; set; }
+
+        public int GroupID { get; set; }
+
+        public int RoomId { get; set; }
+    }
+}
