e55d48ccbec8dae46f6f953f505f10b56ca3bd8c
Valery Kardel <kardelw@gmail.com>
2019-03-19 12:55:20 +0300

    Add interfaces

---
+++ SOAProjects/CurriculumProject/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using CurriculumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurriculumProject.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IRepository<Department> Departments { get; set; }
+        IRepository<Faculty> Faculties { get; set; }
+        IRepository<Speciality> Specialities { get; set; }
+        IRepository<Subject> Subjects { get; set; }
+    }
+}
---
+++ SOAProjects/CurriculumProject/Repositories/FacultyRepository.cs
@@ -0,0 +1,54 @@
+﻿using CurriculumProject.Context;
+using CurriculumProject.Interfaces;
+using CurriculumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurriculumProject.Repositories
+{
+    public class FacultyRepository : IRepository<Faculty>
+    {
+        private CurriculumContext db;
+
+        public FacultyRepository(CurriculumContext context)
+        {
+            db = context;
+        }
+
+        public void Create(Faculty faculty)
+        {
+            db.Faculties.Add(faculty);
+        }
+
+        public void Delete(int id)
+        {
+            Faculty faculty = db.Faculties.Find(id);
+            if (faculty != null)
+            {
+                db.Faculties.Remove(faculty);
+            }
+        }
+
+        public IEnumerable<Faculty> Find(Func<Faculty, bool> predicate)
+        {
+            return db.Faculties.Where(predicate).ToList();
+        }
+
+        public Faculty Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Faculty> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Faculty item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
