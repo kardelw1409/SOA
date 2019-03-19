e1a25e1c59c6206788617b726ae203b4b8e48952
Valery Kardel <kardelw@gmail.com>
2019-03-20 01:48:25 +0300

    Add infrastructure

---
+++ SOAProjects/CurriculumProject/Infrastructure/ServiceModule.cs
@@ -0,0 +1,23 @@
+﻿using CurriculumProject.Interfaces;
+using CurriculumProject.Repositories;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurriculumProject.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
---
+++ SOAProjects/CurriculumProject/Interfaces/IFacultyServices.cs
@@ -0,0 +1,16 @@
+﻿using CurriculumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurriculumProject.Interfaces
+{
+    public interface IFacultyServices
+    {
+        Faculty GetFaculty(int? id);
+        IEnumerable<Faculty> GetFaculties();
+        void Dispose();
+    }
+}
