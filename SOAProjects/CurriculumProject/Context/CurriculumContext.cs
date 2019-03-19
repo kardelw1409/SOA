58e9c2368b5298054acbfc8c26c0dbb8e96bb1e6
Valery Kardel <kardelw@gmail.com>
2019-03-19 12:14:25 +0300

    Add context

---
+++ SOAProjects/CurriculumProject/Context/CurriculumContext.cs
@@ -0,0 +1,22 @@
+﻿using CurriculumProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CurriculumProject.Context
+{
+    public class CurriculumContext : DbContext
+    {
+        public CurriculumContext(string conectionString) : base(conectionString)
+        {
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Faculty> Faculties { get; set; }
+        public DbSet<Speciality> Specialities { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
