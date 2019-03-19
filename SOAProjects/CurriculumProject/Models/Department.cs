8635907995829d18ad6ad95cf0dc2665018f26c2
Valery Kardel <kardelw@gmail.com>
2019-03-19 10:43:30 +0300

    Create model

---
+++ SOAProjects/CurriculumProject/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurriculumProject.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int FacultyId { get; set; }
+        public virtual Faculty Faculty { get; set; }
+        public virtual ICollection<Subject> Subjects { get; set; }
+
+        public Department()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+}
---
+++ SOAProjects/CurriculumProject/Models/Speciality.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurriculumProject.Models
+{
+    public class Speciality
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Plan { get; set; }
+
+        public int FacultyId { get; set; }
+        public virtual Faculty Faculty { get; set; }
+        //public virtual ICollection<Group> Groups { get; set; }
+        //public virtual ICollection<Entrant> Entrants { get; set; }
+
+        public Speciality()
+        {
+            //Entrants = new List<Entrant>();
+            //Groups = new List<Group>();
+        }
+    }
+}
