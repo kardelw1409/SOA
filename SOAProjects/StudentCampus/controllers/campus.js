2fc87d42e70a864c9abdffd3604976dc5594725b
Valery Kardel <kardelw@gmail.com>
2019-03-27 03:57:49 +0300

    Create StudentCampus project, create controllers/models directory

---
+++ SOAProjects/StudentCampus/controllers/campus.js
@@ -0,0 +1,42 @@
+﻿var Campus = require('../models/campus');
+
+exports.test = function (req, res) {
+    res.send('Greetings from Campus controller!');
+};
+
+exports.campus_create = function (req, res) {
+    var campus = new Campus(
+        {
+            id: req.body.id,
+            adress: req.body.adress
+        }
+    );
+
+    campus.save(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.send('Campus Created successfully')
+    })
+};
+
+exports.campus_details = function (req, res) {
+    Campus.findById(req.params.id, function (err, campus) {
+        if (err) return next(err);
+        res.send(campus);
+    })
+};
+
+exports.campus_update = function (req, res) {
+    Campus.findByIdAndUpdate(req.params.id, { $set: req.body }, function (err, campus) {
+        if (err) return next(err);
+        res.send('Campus udpated.');
+    });
+};
+
+exports.Campus_delete = function (req, res) {
+    Campus.findByIdAndRemove(req.params.id, function (err) {
+        if (err) return next(err);
+        res.send('Deleted successfully!');
+    })
+};
---
+++ SOAProjects/StudentCampus/models/room.js
@@ -0,0 +1,13 @@
+﻿var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var RoomSchema = new Schema({
+    id: { type: Number, required: true },
+    number: { type: Number, required: true },
+    place_count: { type: Number, required: true },
+    campus_id: { type: Number, required: true }
+});
+
+
+// Export the model
+module.exports = mongoose.model('Room', RoomSchema);
