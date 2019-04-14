84d3171fcea17d4b02ccfbf1a5ee16b467633d31
Russzh <47798938+Russzh@users.noreply.github.com>
2019-04-14 22:26:05 +0300

    try to add 'one to one'

--- SOAProjects/CompanyService/Profile/profile_model.js
+++ SOAProjects/CompanyService/Profile/profile_model.js
@@ -5,7 +5,8 @@ const ProfileSchema = new Schema({
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     patronymic: { type: String, required: true },
-    birthday: { type: String, required: true }
+    birthday: { type: String, required: true },
+    profile: { collection: 'profile', via: 'owner' }
    // rooms: [{ type: Schema.Types.ObjectId, ref: 'Room' }]
 });
 
--- SOAProjects/CompanyService/entrant/entrant_model.js
+++ SOAProjects/CompanyService/entrant/entrant_model.js
@@ -6,7 +6,11 @@ const EntrantSchema = new Schema({
     medal: { type: Boolean, required: true },
     total_score: { type: Number, required: true },
     is_entranted: { type: Boolean, required: true },
-    entrant_year: { type: Number, required: true }
+    entrant_year: { type: Number, required: true },
+    owner: {
+        model: 'Profile',
+        unique: true
+    }
    /// _speciality_id: { type: Schema.Types.ObjectId, ref: 'brbrbbr' }
 });
 
